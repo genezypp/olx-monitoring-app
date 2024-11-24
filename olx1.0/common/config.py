bbf716587c624cbd792e9225d84878941551c46a
genezypp <investbart5@gmail.com>
2024-11-24 13:49:25 +0100

    .

--- olx1.0/common/config.py
+++ olx1.0/common/config.py
@@ -1,5 +1,4 @@
-# Œcie¿ka do bazy danych SQLite
-DATABASE_URL = "olx_app.db"
-OLX_API_URL = "https://api.olx.pl"
-CLIENT_ID = "YOUR_CLIENT_ID"        # Wstaw swój client_id
-CLIENT_SECRET = "YOUR_CLIENT_SECRET"  # Wstaw swój client_secret
+import os
+
+# Œcie¿ka do pliku SQLite
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///olx_app.db")

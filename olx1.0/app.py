81bc39aff00bc031c812167be221d4e318fa5b30
genezypp <investbart5@gmail.com>
2024-11-24 11:15:04 +0100

    zmiana w fastapi

--- olx1.0/app.py
+++ olx1.0/app.py
@@ -1,9 +1,17 @@
-from fastapi import Request, Query
+from fastapi import FastAPI, Request, Query
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from data_manager import fetch_filtered_ads
 
+# Tworzenie instancji FastAPI
+app = FastAPI()
+
+# Ustawienia Jinja2
 templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/ads", response_class=HTMLResponse)
 async def show_ads(request: Request,
@@ -11,6 +19,5 @@ async def show_ads(request: Request,
                    min_price: float = Query(None),
                    max_price: float = Query(None),
                    location: str = Query(None)):
-    """Wyswietla ogloszenia z bazy z mozliwoscia filtrowania."""
     ads = fetch_filtered_ads(keyword, min_price, max_price, location)
     return templates.TemplateResponse("ads.html", {"request": request, "ads": ads})
